15b1320f4633c48905475a624d9789b34f01308e
anderson <sp726129246@gmail.com>
2018-03-28 22:20:42 +0800

    jwt config over

--- FreshShop/settings.py
+++ FreshShop/settings.py
@@ -161,3 +161,10 @@ REST_FRAMEWORK = {
         'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
     )
 }
+
+import datetime
+
+JWT_AUTH = {
+    'JWT_EXPIRATION_DELTA': datetime.timedelta(days=7),
+    'JWT_AUTH_HEADER_PREFIX': 'JWT',
+}
